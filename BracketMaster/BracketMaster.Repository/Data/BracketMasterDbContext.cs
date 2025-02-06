2add20aaec889ee16bda351e6df8decdc84cce8d
Bogdán Krisztián <bogdan.krisztian17@gmail.com>
2025-02-06 20:20:28 +0100

    Added service-based database

---
+++ BracketMaster/BracketMaster.Repository/Data/BracketMasterDbContext.cs
@@ -0,0 +1,37 @@
+﻿using BracketMaster.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketMaster.Repository
+{
+    public class BracketMasterDbContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Tournament> Tournaments { get; set; }
+
+        public BracketMasterDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=P:\\Projects\\c#\\BracketMaster\\BracketMaster\\BracketMaster.Repository\\Database\\BracketMasterDb.mdf;Integrated Security=True;MultipleActiveResultSets=True")
+                    .UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // kapcsolatok
+        }
+    }
+}
