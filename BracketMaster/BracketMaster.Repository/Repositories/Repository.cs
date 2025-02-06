0ad58b397381ad447bd588e5681078afb4592367
Bogdán Krisztián <bogdan.krisztian17@gmail.com>
2025-02-06 20:22:48 +0100

    Added Repository with its interfaca

---
+++ BracketMaster/BracketMaster.Repository/Repositories/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketMaster.Repository
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        protected BracketMasterDbContext ctx;
+
+        public Repository(BracketMasterDbContext ctx)
+        {
+            this.ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public virtual T Read(int id)
+        {
+            return ctx.Set<T>().FirstOrDefault(item => item.Id == id);
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public virtual void Update(T item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                    prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
