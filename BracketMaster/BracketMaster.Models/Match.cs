1fc0c7354223e3e79a9dc3aabe6ebcf0ef101bbb
Bogdán Krisztián <bogdan.krisztian17@gmail.com>
2025-02-06 19:54:24 +0100

    Added Models

---
+++ BracketMaster/BracketMaster.Models/Match.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketMaster.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public Tournament Tournament { get; set; }
+
+        public Player Home { get; set; }
+        public Player Away { get; set; }
+
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+
+        public bool IsFinished { get { return HomeScore >= 10 || AwayScore >= 10; } }
+        public bool IsOverTime { get { return HomeScore >= 10 && AwayScore >= 10; } }
+
+        public Player Winner { get { return HomeScore > AwayScore ? Home : Away; } }
+        public int CupDifference { get { return Math.Abs(HomeScore - AwayScore); } }
+    }
+}
---
+++ BracketMaster/BracketMaster.Models/Tournament.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BracketMaster.Models
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+        public List<Match> Matches { get; set; }
+
+        public Tournament()
+        {
+            Players = new List<Player>();
+            Matches = new List<Match>();
+        }
+    }
+}
